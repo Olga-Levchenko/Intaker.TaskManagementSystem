3c5c673a826d364823d67c83fd19a9dd4aa9c1c6
Olga Levchenko <olgakazakovskaya@ukr.net>
2025-01-03 09:50:08 +0200

    Migrate database on creating db context in order to avoid manual migration before project run

--- src/Intaker.TaskManagementSystem.DataAccess/TasksDbContext.cs
+++ src/Intaker.TaskManagementSystem.DataAccess/TasksDbContext.cs
@@ -3,8 +3,13 @@ using Task = Intaker.TaskManagementSystem.DataAccess.Entities.Task;
 
 namespace Intaker.TaskManagementSystem.DataAccess
 {
-    public class TasksDbContext(DbContextOptions<TasksDbContext> options) : DbContext(options)
+    public class TasksDbContext : DbContext
     {
         public required DbSet<Task> Tasks { get; set; }
+
+        public TasksDbContext(DbContextOptions<TasksDbContext> options) : base(options)
+        {
+            Database.Migrate();
+        }
     }
 }
